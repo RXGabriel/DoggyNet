1b80890a6715e9f64e7e1272da917ad060274edb
RXGabriel <gabriel2005rf@gmail.com>
2024-06-10 15:43:46 -0300

    feat: add Feed component with FeedPhotos

---
+++ src/components/feed/feed.tsx
@@ -0,0 +1,9 @@
+import FeedPhotos from "./feed-photos";
+
+export default function Feed({ photos }: { photos: any }) {
+  return (
+    <div>
+      <FeedPhotos photos={photos} />
+    </div>
+  );
+}

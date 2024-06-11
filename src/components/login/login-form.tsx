25be9af69e371d106cdb48299d2e2d89b04f4a57
RXGabriel <gabriel2005rf@gmail.com>
2024-06-11 15:55:38 -0300

    feat: enhance login form with additional links and styles

--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 "use client";
 
 import login from "@/actions/login";
@@ -6,6 +7,8 @@ import Button from "@/components/forms/button";
 import Input from "@/components/forms/input";
 import ErrorMessage from "../helper/error-message";
 import React from "react";
+import Link from "next/link";
+import styles from "./login-form.module.css";
 
 function FormButton() {
   const { pending } = useFormStatus();
@@ -34,12 +37,24 @@ export default function LoginForm() {
 
   return (
     <>
-      <form action={action}>
+      <form action={action} className={styles.form}>
         <Input label="User" name="username" type="text" />
         <Input label="Password" name="password" type="password" />
         <ErrorMessage error={state.error} />
         <FormButton />
       </form>
+
+      <Link className={styles.lost} href="/login/lost">
+        Forgot password?
+      </Link>
+
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Sign up </h2>
+        <p>Don't have an account? Sign up</p>
+        <Link className="button" href="/login/create">
+          Sign up
+        </Link>
+      </div>
     </>
   );
 }

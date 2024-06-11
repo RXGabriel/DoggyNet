b8459206e8f725b7d8917b8ae9f9ccfba866f903
RXGabriel <gabriel2005rf@gmail.com>
2024-06-11 15:33:19 -0300

    feat: enhance LoginForm component with Input and ErrorMessage components

--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -3,6 +3,9 @@
 import login from "@/actions/login";
 import { useFormState, useFormStatus } from "react-dom";
 import Button from "@/components/forms/button";
+import Input from "@/components/forms/input";
+import ErrorMessage from "../helper/error-message";
+import React from "react";
 
 function FormButton() {
   const { pending } = useFormStatus();
@@ -25,11 +28,16 @@ export default function LoginForm() {
     data: null,
   });
 
+  React.useEffect(() => {
+    if (state.ok) window.location.href = "/account";
+  }, [state.ok]);
+
   return (
     <>
       <form action={action}>
-        <input type="text" name="username" placeholder="User" />
-        <input type="password" name="password" placeholder="Password" />
+        <Input label="User" name="username" type="text" />
+        <Input label="Password" name="password" type="password" />
+        <ErrorMessage error={state.error} />
         <FormButton />
       </form>
     </>

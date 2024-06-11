591cb410de6df597b35393ea1c62aa45f552f531
RXGabriel <gabriel2005rf@gmail.com>
2024-06-11 18:30:42 -0300

    refactor: Wrap LoginPage content in a div element for better styling flexibility

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,9 +2,9 @@ import LoginForm from "@/components/login/login-form";
 
 export default async function LoginPage() {
   return (
-    <section className="animeLeft">
+    <div className="animeLeft">
       <h1 className="title">Login</h1>
       <LoginForm />
-    </section>
+    </div>
   );
 }

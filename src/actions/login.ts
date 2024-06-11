55418a2da72956b2e06eca7efe47a34545b1bd31
RXGabriel <gabriel2005rf@gmail.com>
2024-06-11 11:17:15 -0300

    feat: enhance login function with error handling and validation

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,24 +1,31 @@
 "use server";
-
+import { TOKEN_POST } from "@/functions/api";
+import apiError from "@/functions/api-error";
 import { cookies } from "next/headers";
 
-export default async function login(formData: FormData) {
+export default async function login(state: {}, formData: FormData) {
   const username = formData.get("username") as string | null;
   const password = formData.get("password") as string | null;
+  const URL = TOKEN_POST();
 
-  const response = await fetch(
-    "https://dogsapi.origamid.dev/json/jwt-auth/v1/token",
-    {
+  try {
+    if (!username || password) throw new Error("Fill all fields");
+    const response = await fetch(URL, {
       method: "POST",
       body: formData,
-    }
-  );
-  const data = await response.json();
-  cookies().set("token", data.token, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "lax",
-    maxAge: 60 * 60 * 24,
-  });
-  console.log(data);
+    });
+
+    if (!response.ok) throw new Error("Invalid username or password");
+
+    const data = await response.json();
+    cookies().set("token", data.token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "lax",
+      maxAge: 60 * 60 * 24,
+    });
+    return { ok: true, error: "", data: null };
+  } catch (error: unknown) {
+    return apiError(error);
+  }
 }

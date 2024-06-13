293803350073ef028fbce92f07845a2aea7f3943
RXGabriel <gabriel2005rf@gmail.com>
2024-06-13 10:33:59 -0300

    feat: integrate user context provider into RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,25 +3,32 @@ import "./globals.css";
 import { type_second } from "@/functions/fonts";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import { UserContextProvider } from "@/context/user-context";
+import userGet from "@/actions/user-get";
+import { transformUser } from "@/utils/transformUser";
 
 export const metadata: Metadata = {
   title: "Doggy Net",
   description: "Social network for dogs",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { data: userFromServer } = await userGet();
+  const user = transformUser(userFromServer);
   return (
     <html lang="en">
       <body className={type_second.variable}>
-        <div className="App">
-          <Header />
-          <main className="AppBody">{children}</main>
-          <Footer />
-        </div>
+        <UserContextProvider user={user}>
+          <div className="App">
+            <Header />
+            <main className="AppBody">{children}</main>
+            <Footer />
+          </div>
+        </UserContextProvider>
       </body>
     </html>
   );

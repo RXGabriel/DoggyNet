2928e0f4fe8906d03441b923177739700999ab26
RXGabriel <gabriel2005rf@gmail.com>
2024-06-13 10:32:49 -0300

    refactor: integrate user context into AccountPage component

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,7 +1,13 @@
-export default async function AccountPage() {
+"use client";
+
+import { useUser } from "@/context/user-context";
+
+export default function AccountPage() {
+  const { user } = useUser();
+
   return (
     <main>
-      <h1>Account</h1>
+      <h1>Account: {user?.nome}</h1>
     </main>
   );
 }

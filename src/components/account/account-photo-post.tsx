70cffecb0c00bd26ea1d11e7b8766c37185dd3bc
RXGabriel <gabriel2005rf@gmail.com>
2024-06-13 16:43:00 -0300

    feat: add AccountPhotoPost component for photo uploads

---
+++ src/components/account/account-photo-post.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useFormState, useFormStatus } from "react-dom";
+import Button from "@/components/forms/button";
+import Input from "@/components/forms/input";
+import ErrorMessage from "../helper/error-message";
+import { ChangeEvent, useState } from "react";
+import styles from "./account-photo-post.module.css";
+import PhotoPost from "@/actions/photos-post";
+
+function FormButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button type="submit" disabled={pending} aria-label="Send">
+      {pending ? "Sending..." : "Send"}
+    </Button>
+  );
+}
+
+export default function AccountPhotoPost() {
+  const [state, action] = useFormState(PhotoPost, {
+    ok: false,
+    error: "",
+    data: null,
+  });
+  const [img, setImg] = useState("");
+
+  function handleImgChange({ target }: ChangeEvent<HTMLInputElement>) {
+    if (target.files) setImg(URL.createObjectURL(target.files[0]));
+  }
+
+  return (
+    <section className={`${styles.photoPost} animeLeft`}>
+      <form action={action} className={styles.form}>
+        <Input label="Name" name="name" type="text" />
+        <Input label="Weight" name="weight" type="number" />
+        <Input label="Age" name="age" type="number" />
+
+        <input
+          onChange={handleImgChange}
+          type="file"
+          name="img"
+          id="img"
+          className={styles.file}
+        />
+        <ErrorMessage error={state.error} />
+        <FormButton />
+      </form>
+
+      <div>
+        {img && (
+          <div
+            className={styles.preview}
+            style={{ backgroundImage: `url('${img}')` }}
+          ></div>
+        )}
+      </div>
+    </section>
+  );
+}

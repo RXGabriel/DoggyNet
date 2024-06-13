a04123a179c698094e718e32c3b9e9acce664924
RXGabriel <gabriel2005rf@gmail.com>
2024-06-13 10:31:22 -0300

    feat: add user context provider and hook

---
+++ src/context/user-context.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { User } from "@/interfaces/user";
+import {
+  createContext,
+  useContext,
+  useState,
+  type Dispatch,
+  type ReactNode,
+  type SetStateAction,
+} from "react";
+
+interface UserContextType {
+  user: User | null;
+  setUser: Dispatch<SetStateAction<User | null>>;
+}
+
+interface UserContextProviderProps {
+  children: ReactNode;
+  user: User | null;
+}
+
+const UserContext = createContext<UserContextType | null>(null);
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (!context)
+    throw new Error("userContext must be used within a UserProvider");
+
+  return context;
+};
+
+export function UserContextProvider({
+  children,
+  user,
+}: UserContextProviderProps) {
+  const [userState, setUser] = useState<User | null>(user);
+
+  return (
+    <UserContext.Provider value={{ user: userState, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+}

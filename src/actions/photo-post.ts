6b6ad2b32da6e17cedb846d1e5cf25ba92b01430
RXGabriel <gabriel2005rf@gmail.com>
2024-06-14 15:38:31 -0300

    fix: correct form data parameter names in photoPost function

--- src/actions/photo-post.ts
+++ src/actions/photo-post.ts
@@ -10,27 +10,25 @@ import { redirect } from "next/navigation";
 export default async function photoPost(state: State, formData: FormData) {
   const token = cookies().get("token")?.value;
 
-  const name = formData.get("name") as string | null;
-  const age = formData.get("age") as string | null;
-  const weight = formData.get("weight") as string | null;
+  const name = formData.get("nome") as string | null;
+  const age = formData.get("idade") as string | null;
+  const weight = formData.get("peso") as string | null;
   const img = formData.get("img") as File;
   const URL = PHOTO_POST();
 
   try {
-    if (!token) throw new Error("Token not found.");
+    if (!token) throw new Error("Token not found");
 
     if (!name || !age || !weight || img.size === 0)
-      throw new Error("Fill all fields");
+      throw new Error("Fill al fields");
 
     const response = await fetch(URL, {
       method: "POST",
-      headers: {
-        authorization: `Bearer ${token}`,
-      },
+      headers: { authorization: `Bearer ${token}` },
       body: formData,
     });
 
-    if (!response.ok) throw new Error("Error on post photo");
+    if (!response.ok) throw new Error("Error on send photo. ");
   } catch (error: unknown) {
     return apiError(error);
   }

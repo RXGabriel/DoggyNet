04bd89837dbc7e2b30a2eb788dde36bfb773ecad
RXGabriel <gabriel2005rf@gmail.com>
2024-06-17 11:46:31 -0300

    refactor: update AccountPage to fetch user data and photos on load

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,13 +1,37 @@
+import photosGet from "@/actions/photos-get";
+import userGet from "@/actions/user-get";
+import Feed from "@/components/feed/feed";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: "My account | Doggy Net",
+  title: "My Account",
 };
 
-export default function AccountPage() {
+export default async function AccountPage() {
+  const { data: user } = await userGet();
+  const { data: photos } = await photosGet({ user: user?.username });
+
   return (
-    <main>
-      <h1>Account</h1>
-    </main>
+    <section>
+      {photos?.length ? (
+        <Feed photos={photos} />
+      ) : (
+        <span>
+          <p
+            style={{ color: "#444", fontSize: "1.25rem", marginBottom: "1rem" }}
+          >
+            There is no photos yet. Post one to get started!
+          </p>
+          <Link
+            href="/account/post"
+            style={{ display: "inline-block" }}
+            className="button"
+          >
+            Post a new photo
+          </Link>
+        </span>
+      )}
+    </section>
   );
 }

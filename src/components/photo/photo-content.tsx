f987603cb266f2e071b7f825dac1108a179f66c0
RXGabriel <gabriel2005rf@gmail.com>
2024-06-17 17:16:49 -0300

    feat: add PhotoContent component

---
+++ src/components/photo/photo-content.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import PhotoDelete from "@/components/photo/photo-delete";
+import { useUser } from "@/context/user-context";
+import { PhotoData } from "@/interfaces/photo";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./photo-content.module.css";
+
+const PhotoContent = ({
+  data,
+  single,
+}: {
+  data: PhotoData;
+  single: boolean;
+}) => {
+  const { user } = useUser();
+  const { photo, comments } = data;
+
+  return (
+    <div className={`${styles.photo} ${single ? styles.single : ""}`}>
+      <div className={styles.img}>
+        <Image
+          src={photo.src}
+          alt={photo.title}
+          title={photo.title}
+          width={1000}
+          height={1000}
+          sizes="100vw"
+        />
+      </div>
+      <div className={styles.details}>
+        <div>
+          <p className={styles.author}>
+            {user && user.username === photo.author ? (
+              <PhotoDelete id={String(photo.id)} />
+            ) : (
+              <Link href={`/profile/${photo.author}`}>@{photo.author}</Link>
+            )}
+            <span className={styles.visualizacoes}>{photo.acessos}</span>
+          </p>
+          <h1 className="title">
+            <Link href={`/photo/${photo.id}`}>{photo.title}</Link>
+          </h1>
+          <ul className={styles.attributes}>
+            <li>{photo.peso} lb </li>
+            <li>
+              {photo.idade === "1"
+                ? `${photo.idade} year`
+                : `${photo.idade} years`}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PhotoContent;

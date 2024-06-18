3b4d5852a4e842f3a34133f33d081d4c04449d4a
RXGabriel <gabriel2005rf@gmail.com>
2024-06-18 16:03:19 -0300

    feat: add account statistics on statistics page

--- src/app/account/statistics/page.tsx
+++ src/app/account/statistics/page.tsx
@@ -1,13 +1,27 @@
+import statsGet from "@/actions/stats-get";
 import { Metadata } from "next";
+import dynamic from "next/dynamic";
+
+const AccountStatistics = dynamic(
+  () => import("@/components/account/account-statistics"),
+  {
+    loading: () => <p>Loading...</p>,
+    ssr: false,
+  }
+);
 
 export const metadata: Metadata = {
-  title: "Statistics | Doggy Net",
+  title: "Statistics | My Account",
 };
 
 export default async function StatisticPage() {
+  const { data } = await statsGet();
+
+  if (!data) return null;
+
   return (
-    <main>
-      <h1>Statistics</h1>
-    </main>
+    <section>
+      <AccountStatistics data={data} />
+    </section>
   );
 }
